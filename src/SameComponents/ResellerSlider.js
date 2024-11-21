9d00c7b95317a23d721f3e35245ae1048b42b76c
Faisal Khan <faisalkh01122@gmail.com>
2024-11-21 09:32:54 -0800

    ww

--- src/SameComponents/ResellerSlider.js
+++ src/SameComponents/ResellerSlider.js
@@ -43,13 +43,13 @@ const ResellerSlider = () => {
                         }}
                     >
                         <SwiperSlide>
-                            <img src="https://assets.tripplite.com/large-image/sr48ub-front-l.jpg" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://assets.tripplite.com/large-image/sr48ub-front-l.jpg" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
 
                         <SwiperSlide>
-                            <img src="https://stl.tech/wp-content/uploads/2023/01/Routers-in-networking.jpg" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://stl.tech/wp-content/uploads/2023/01/Routers-in-networking.jpg" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
@@ -57,50 +57,50 @@ const ResellerSlider = () => {
 
 
                         <SwiperSlide>
-                            <img src="https://www.phoenix.com/wp-content/uploads/green-circuit-board.jpg" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
-                         
-                            <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
-                        </SwiperSlide>
-
-
-                        <SwiperSlide>
-                            <img src="https://5.imimg.com/data5/TE/DC/GM/SELLER-1683937/local-control-panels-lcp--500x500.jpg" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://www.phoenix.com/wp-content/uploads/green-circuit-board.jpg" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
 
 
                         <SwiperSlide>
-                            <img src="https://upsonlinestore.com/wp-content/uploads/2021/05/WPL150-12N-480x480-1.jpg" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://5.imimg.com/data5/TE/DC/GM/SELLER-1683937/local-control-panels-lcp--500x500.jpg" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
 
 
                         <SwiperSlide>
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9cYP5KO9DYNDolcPDOk60JZ-C3RWsdkB80g&s" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://upsonlinestore.com/wp-content/uploads/2021/05/WPL150-12N-480x480-1.jpg" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
 
 
                         <SwiperSlide>
-                            <img src="https://e6f7jzv78yj.exactdn.com/wp-content/uploads/2017/08/Power-Cables-lineup-1170x585.jpg?strip=all&lossy=1&ssl=1" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9cYP5KO9DYNDolcPDOk60JZ-C3RWsdkB80g&s" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
+                         
+                            <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
+                        </SwiperSlide>
+
+
+                        <SwiperSlide>
+                            <img src="https://e6f7jzv78yj.exactdn.com/wp-content/uploads/2017/08/Power-Cables-lineup-1170x585.jpg?strip=all&lossy=1&ssl=1" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9bzXhZKO7f1JkZ3yqQzybkOLz-eIU7VTzsA&s" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9bzXhZKO7f1JkZ3yqQzybkOLz-eIU7VTzsA&s" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
                         <SwiperSlide>
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyvQub-4-h9eNxHwjE9nd7ibYUq-M4mEbF9w&s" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyvQub-4-h9eNxHwjE9nd7ibYUq-M4mEbF9w&s" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide>
                         {/* <SwiperSlide>
-                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyvQub-4-h9eNxHwjE9nd7ibYUq-M4mEbF9w&s" className='h-full' style={{ maxWidth: '160px' , height:'50vh'}} alt="" />
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyvQub-4-h9eNxHwjE9nd7ibYUq-M4mEbF9w&s" className='h-full border border-2 ' style={{ maxWidth: '260px' , height:'50vh'}} alt="" />
                          
                             <button type='button' className='px-2 py-1 text-white fw-bold border-0 mt-3 rounded-3 mb-3' style={{ backgroundColor: '#009BDD' }}>Buy Now</button>
                         </SwiperSlide> */}
